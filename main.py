eff23a733b176d7758fc9b0ca327b901a78ea268
Shyaboi <ian.shane.sears@gmail.com>
2020-05-02 23:46:32 -0700

    dot showing

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+pad_a = turtle.Turtle()
+pad_a.speed(0)
+pad_a.shape('square')
+pad_a.color('white')
+pad_a.penup()
+pad_a.goto(-350,0)
 
 while True:
     wn.update()

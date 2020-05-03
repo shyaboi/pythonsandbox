8ae62efbd7fb800a491767110b1f795f7e73dea6
Shyaboi <ian.shane.sears@gmail.com>
2020-05-02 23:48:53 -0700

    2 paddles

--- main.py
+++ main.py
@@ -6,12 +6,27 @@ wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+
+# pad a
 pad_a = turtle.Turtle()
 pad_a.speed(0)
 pad_a.shape('square')
 pad_a.color('white')
+pad_a.shapesize(stretch_wid=5, stretch_len=1)
 pad_a.penup()
 pad_a.goto(-350,0)
+
+# pad b
+pad_a = turtle.Turtle()
+pad_a.speed(0)
+pad_a.shape('square')
+pad_a.color('white')
+pad_a.shapesize(stretch_wid=5, stretch_len=1)
+pad_a.penup()
+pad_a.goto(350,0)
+
+
+
 
 while True:
     wn.update()
